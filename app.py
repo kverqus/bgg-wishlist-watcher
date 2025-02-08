469e8eb9400b2e4f31c14b7666b672a5a9535da9
kverqus <kristoffer.nilsson@outlook.com>
2025-02-08 22:16:57 +0100

    write wishlist and information to database

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import importlib
 
 from scrapers.base import ScraperBase
 from wishlist import Wishlist
+from database import initialize_db, add_wishlist_item
 
 
-def load_scrapers():
+def load_scrapers() -> dict:
     scrapers = {}
     for finder, name, ispkg in pkgutil.iter_modules(["scrapers"]):
         module = importlib.import_module(f"scrapers.{name}")
@@ -16,12 +17,20 @@ def load_scrapers():
     return scrapers
 
 
+def update_wishlist(wishlist: Wishlist):
+    for item in wishlist.items:
+        add_wishlist_item(item.name)
+
+
 if __name__ == '__main__':
+    initialize_db()
     scrapers = load_scrapers()
     wishlist = Wishlist(username="Ojeu")
     wishlist.get_wishlist()
 
+    update_wishlist(wishlist)
+
     for name, scraper in scrapers.items():
         for item in wishlist.items:
-            result = scraper.search(item.name)
+            result = scraper.safe_search(item.name)
             print(f"{name}: {result}")
