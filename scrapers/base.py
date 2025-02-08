7b85ade2365f7150c4fd7eee14484aea0ee06186
kverqus <kristoffer.nilsson@outlook.com>
2025-02-08 22:15:19 +0100

    method for scraper which includes error handling, logging and saving to database

--- scrapers/base.py
+++ scrapers/base.py
@@ -1,8 +1,36 @@
+import logging
+
 from abc import ABC, abstractmethod
+
+from database import save_game_result
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ScraperBase(ABC):
+    store_name = None
+
     @abstractmethod
     def search(self, game_name: str) -> None:
         """Search for the board game and return price and availability."""
         pass
+
+    def safe_search(self, wishlist_name):
+        """Wrapper to log execution, handle errors, and save results."""
+        try:
+            logger.info(f"Searching for game: {wishlist_name}")
+            result = self.search(wishlist_name)
+            logger.info(f"Search successful: {result}")
+
+            if result:
+                for item in result:
+                    save_game_result(wishlist_name, self.store_name,
+                                    item['name'], item['price'], item['availability'], item['url'])
+
+            return result
+
+        except Exception as e:
+            logger.error(f"Error searching for {wishlist_name}: {e}")
+            return {"error": "Failed to retrieve game data"}
